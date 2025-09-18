52674055bbf2c05a145f83cc830ab9a46c1a6e23
Peter Snell <flibisk@Mac.attlocal.net>
2025-09-17 20:25:02 -0500

    Remove ICO favicon, use PNG only for modern browser compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
